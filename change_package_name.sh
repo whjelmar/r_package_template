724f98a70ed79060d55efae727a61a12c63a3332
Walter Hjelmar <walter@hjelmar.com>
2021-04-14 18:28:02 -0400

    fix: bash syntax issues

--- change_package_name.sh
+++ change_package_name.sh
@@ -6,6 +6,7 @@ read -p 'Can you give a 1 sentence description of the purpose for the package? '
 find . -path ./.git -prune -o -type f -exec sed -i "s/<<package_name>>/$package_name/g" {} +
 find . -path ./.git -prune -o -type f -exec sed -i "s/<<package_title>>/$package_title/g" {} +
 
+git mv default_template_name.Rproj "$package_name.Rproj"
 git add --all
 git commit -m "feat: replaced defaults in templates with package info"
 git push
