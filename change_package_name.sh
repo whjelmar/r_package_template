eea2096452d54d2eb75a0e446a8042ace64d4149
Walter Hjelmar <walter@hjelmar.com>
2021-10-05 09:23:31 -0400

    Update change_package_name.sh

--- change_package_name.sh
+++ change_package_name.sh
@@ -3,8 +3,8 @@
 read -p 'What would you like to name this new package? ' package_name
 read -p 'Can you give a 1 sentence description of the purpose for the package? ' package_title
 
-find . -path ./.git -prune -o -type f -exec sed -i "s/rolemapr/$package_name/g" {} +
-find . -path ./.git -prune -o -type f -exec sed -i "s/A package to produce roles and responsibility maps/$package_title/g" {} +
+find . -path ./.git -prune -o -type f -exec sed -i "s/<<package_name>>/$package_name/g" {} +
+find . -path ./.git -prune -o -type f -exec sed -i "s/<<package_title>>/$package_title/g" {} +
 
 git mv default_template_name.Rproj "$package_name.Rproj"
 git add --all
